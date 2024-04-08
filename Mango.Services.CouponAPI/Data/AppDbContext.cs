ca7830c37fda2307b7d95a70d647e845fe8e07dc
Siarhei Biadniuk <sergeybednyuk@gmail.com>
2024-04-08 16:39:50 +0200

    do migration to DB for Mango_CouponApi

--- Mango.Services.CouponAPI/Data/AppDbContext.cs
+++ Mango.Services.CouponAPI/Data/AppDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace Mango.Services.CouponAPI.Data
+﻿using Mango.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponAPI.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
     }
 }
--- Mango.Services.CouponAPI/Program.cs
+++ Mango.Services.CouponAPI/Program.cs
@@ -1,4 +1,7 @@
 
+using Mango.Services.CouponAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace Mango.Services.CouponAPI
 {
     public class Program
@@ -8,7 +11,10 @@ namespace Mango.Services.CouponAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            builder.Services.AddDbContext<AppDbContext>(option => 
+            {
+                option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
